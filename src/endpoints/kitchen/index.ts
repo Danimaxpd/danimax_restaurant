d748e77fd28a50fd137dcf6d16c35586f4a057ae
David Jimenez <danimax.com@gmail.com>
2023-08-20 17:02:28 -0500

    Updated Create order - now can insert to database - return the result of the database operation.

--- src/endpoints/kitchen/index.ts
+++ src/endpoints/kitchen/index.ts
@@ -1,38 +1,62 @@
 require("dotenv").config();
 
 import { v4 as uuidv4 } from "uuid";
-import { APIGatewayProxyResult } from "aws-lambda";
+import { APIGatewayProxyResult, Context } from "aws-lambda";
 import SQSUtils from "../../utils/sqs";
+import { connectToDB } from "../../utils/mongo";
+import { getRandomRecipe } from "src/utils/randomRecipe";
 
 export default class OrdersHandler {
-  public static async createOrder(): Promise<APIGatewayProxyResult> {
-    const response = {
-      result: "success",
-      message: "Order generated successfully",
-    };
-    SQSUtils.sendMessage(
-      process.env.WAREHOUSE_QUEUE_URL,
-      JSON.stringify({
-        MessageGroupId: "warehouse",
-        MessageDeduplicationId: uuidv4(),
-        MessageBody: `${new Date().toISOString()} - warehouse_order}`,
-      }),
-      "warehouse",
-      {
-        Title: {
-          DataType: "String",
-          StringValue: "warehouse_order",
+  public static async createOrder(
+    context: Context,
+  ): Promise<APIGatewayProxyResult> {
+    // wait for the event loop to be empty before ending the lambda function
+    context.callbackWaitsForEmptyEventLoop = false;
+
+    try {
+      const db = await connectToDB(process.env.DB_URL);
+      const randomRecipeData = await getRandomRecipe();
+      const recipeData = {
+        ...randomRecipeData,
+        status: "new-order",
+        createDate: new Date(),
+        updateDate: new Date(),
+      };
+      const order = await db.collection("orders").insertOne(recipeData);
+      const response = {
+        result: "success",
+        message: order,
+      };
+
+      SQSUtils.sendMessage(
+        process.env.WAREHOUSE_QUEUE_URL,
+        JSON.stringify({
+          MessageGroupId: "warehouse",
+          MessageDeduplicationId: uuidv4(),
+          MessageBody: JSON.stringify(order),
+        }),
+        "warehouse",
+        {
+          Title: {
+            DataType: "String",
+            StringValue: "warehouse_order",
+          },
+          Author: {
+            DataType: "String",
+            StringValue: "kitchen",
+          },
         },
-        Author: {
-          DataType: "String",
-          StringValue: "kitchen",
-        },
-      },
-    );
-    return {
-      statusCode: 201,
-      body: JSON.stringify(response),
-    };
+      );
+      return {
+        statusCode: 201,
+        body: JSON.stringify(response),
+      };
+    } catch (error) {
+      return {
+        statusCode: 400,
+        body: error.message,
+      };
+    }
   }
 
   public static async listOrdersHandler(): Promise<APIGatewayProxyResult> {
